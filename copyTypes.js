1a5c0a9c8c99bf5e543e3b85f791e489128f29b7
Paul Bratslavsky <paul.bratslavsky@gmail.com>
2023-10-18 01:30:22 -0500

    Added Strapi Typescript Support; Created a copyTypes.js script responsible for copying Strapi types to frontend;  types.js file has all the types and interface required to type responses.  I still need to make a readme file and video outlining the workflow.  I refactored the page.tsx to use Strapi types, so you should now see type intelisense.

---
+++ copyTypes.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+const sourcePath = path.join(__dirname, './backend/types/generated/contentTypes.d.ts');
+const destinationPath = path.join(__dirname, './frontend/src/types/contentTypes.d.ts');
+const destinationDir = path.dirname(destinationPath);
+
+// Check if source file exists
+if (!fs.existsSync(sourcePath)) {
+    console.error(`Source file does not exist: ${sourcePath}`);
+    process.exit(1);
+}
+
+// Ensure destination directory exists or create it
+if (!fs.existsSync(destinationDir)) {
+    fs.mkdirSync(destinationDir, { recursive: true });
+}
+
+// Read the source file, modify its content and write to the destination file
+const content = fs.readFileSync(sourcePath, 'utf8');
+const modifiedContent = content.replace('@strapi/strapi', '@strapi/types');
+
+fs.writeFile(destinationPath, modifiedContent, (err) => {
+    if (err) {
+        console.error(`Error writing to destination file: ${err}`);
+        process.exit(1);
+    } else {
+        console.log('File copied and modified successfully!');
+    }
+});
--- frontend/src/types/contentTypes.d.ts
+++ frontend/src/types/contentTypes.d.ts
@@ -1,4 +1,4 @@
-import type { Schema, Attribute } from '@strapi/strapi';
+import type { Schema, Attribute } from '@strapi/types';
 
 export interface AdminPermission extends Schema.CollectionType {
   collectionName: 'admin_permissions';
