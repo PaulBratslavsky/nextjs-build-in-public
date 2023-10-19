349697131a97b3123f62ee8b6c7e97fa3ee691cb
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2023-10-19 00:42:40 -0500

    basic home page layout.  Use as referece. (#15)

---
+++ frontend/src/app/register/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function RegisterRoute() {
+  return (
+    <div className="container mx-auto">
+      <h2>Register Route</h2>
+      <p>Add Registration Form. Should have the following fields</p>
+      <ul>
+        <li>username</li>
+        <li>email</li>
+        <li>password</li>
+      </ul>
+    </div>
+  );
+}
