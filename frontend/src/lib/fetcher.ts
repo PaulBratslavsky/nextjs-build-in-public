b9c832ed7ba0f170d27afa678591543f4fadfb6b
Anton Cherry <ztx@ztx.one>
2023-10-21 19:31:11 +0200

    update fetcher.ts

--- frontend/src/lib/fetcher.ts
+++ frontend/src/lib/fetcher.ts
@@ -2,7 +2,7 @@ const fetcher = async (resource: string) => {
   let res = null;
 
   try {
-    res = await fetch(`http://127.0.0.1:1337/api/${resource}`);
+    res = await fetch(`http://localhost:1337/api/${resource}`);
   } catch (error) {
     console.log(`fetcher -- error`, error);
   }
