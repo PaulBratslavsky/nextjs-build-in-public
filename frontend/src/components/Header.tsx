8835d145f948dc5fe64585ac1a61fe15d10ecd7e
Paul Bratslavsky <paul.bratslavsky@gmail.com>
2023-10-23 02:02:25 -0500

    made the header dynami pulling menuitem, image from Strapi

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,35 +1,89 @@
 import { buttonVariants, Button } from "@/components/ui/button";
-import type { APIResponseData } from "@/types/types";
+import type { APIResponse } from "@/types/types";
+import { getStrapiMedia } from "@/lib/api-helpers";
 import Link from "next/link";
 
-const Header = ({ data }: { data: APIResponseData<"api::global.global"> }) => {
-  // TODO: USE THE RESPONSE DATA AND BUILD THE HEADER
-  console.dir(data, { depth: null });
+interface HeroNavItem {
+  id: string;
+  text: string;
+  href: string;
+  isButton: boolean;
+  isExternal: boolean;
+}
+
+interface HeaderProps {
+  data: {
+    id: string;
+    attributes: {
+      mainNav: {
+        id: string;
+        navItem: HeroNavItem[];
+      };
+      secondaryNav: {
+        id: string;
+        navItem: HeroNavItem[];
+      };
+      logo: {
+        id: string;
+        image: {
+          data: {
+            id: string;
+            attributes: {
+              name: string;
+              alternativeText: string;
+              url: string;
+            };
+          };
+        };
+      };
+    };
+  };
+}
+
+const Header = ({ data }: { data: HeaderProps }) => {
+  console.dir(data.data, { depth: null });
+
+  const imageUrl = getStrapiMedia(
+    data.data.attributes.logo.image.data.attributes.url
+  );
+
+  function renderNavItems(navItems: HeroNavItem[]) {
+    return navItems.map((navItem: HeroNavItem) => {
+      const { text, href, isButton } = navItem;
+      if (isButton) {
+        return (
+          <Link
+            href={href}
+            className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary-foreground h-10 px-4 py-2 bg-[#ce1f3a] hover:bg-[#e96a7e]"
+          >
+            {text}
+          </Link>
+        );
+      } else {
+        return (
+          <Link href={href} className={buttonVariants({ variant: "ghost" })}>
+            {text}
+          </Link>
+        );
+      }
+    });
+  }
+
   return (
     <header className="bg-background/80 sticky top-0 z-20 border-b backdrop-blur">
       <div className="flex h-16 px-8 items-center justify-between">
         <div className="flex items-center">
-          <img
-            src="/eventler_logo_color2.png"
-            alt="Eventler logo"
-            className="h-12"
-          />
+          {imageUrl && (
+            <img src={imageUrl} alt="Eventler logo" className="h-12" />
+          )}
         </div>
         <nav className="hidden gap-2 md:flex">
-          <Link href="/" className={buttonVariants({ variant: "ghost" })}>
-            Home
-          </Link>
-          <Link href="/events" className={buttonVariants({ variant: "ghost" })}>
-            Events
-          </Link>
+          {data.data.attributes.mainNav.navItem &&
+            renderNavItems(data.data.attributes.mainNav.navItem)}
         </nav>
         <div className="flex items-center gap-5">
-          <Button color="gray" variant="ghost" aria-label="Theme">
-            Log in
-          </Button>
-          <Button variant="default" className="bg-[#ce1f3a] hover:bg-[#e96a7e]">
-            Sing up
-          </Button>
+          {data.data.attributes.secondaryNav.navItem &&
+            renderNavItems(data.data.attributes.secondaryNav.navItem)}
         </div>
       </div>
     </header>
