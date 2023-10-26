91fb56602aec173c86acc2ba054da7911056047b
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2023-10-26 00:49:50 -0500

    added form placeholders for user profile and create event as well as image field. (#40)

---
+++ frontend/src/app/dashboard/add-event/AddEventForm.tsx
@@ -0,0 +1,142 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useFieldArray, useForm } from "react-hook-form";
+import { ImageField } from "../../../components/ImageField";
+import * as z from "zod";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+
+const eventFormSchema = z.object({
+  location: z
+    .string()
+    .min(2, {
+      message: "Location must be at least 2 characters.",
+    })
+    .max(144, {
+      message: "Location must not be longer than 144 characters.",
+    }),
+
+  description: z.string().max(160).min(4),
+});
+
+type EventFormValues = z.infer<typeof eventFormSchema>;
+
+// This can come from your database or API.
+const defaultValues: Partial<EventFormValues> = {
+  description: "Tell us about your awesome event.",
+};
+
+export function AddEventForm() {
+  const [file, setFile] = useState<File>();
+  const [previewImage, setPreviewImage] = useState<string | null>(null); // Step 1
+
+  const form = useForm<EventFormValues>({
+    resolver: zodResolver(eventFormSchema),
+    defaultValues,
+    mode: "onChange",
+  });
+
+  function onSubmit(data: EventFormValues) {
+    const formData = new FormData();
+
+    // Append each form data
+    for (const [key, value] of Object.entries(data)) {
+      formData.append(key, value);
+    }
+
+    // Append the file
+    if (file) {
+      formData.append("file", file); // 'file' here is the field name for the file, you can name it as per your backend's requirements
+    }
+
+    // Assuming you would be making an HTTP request, this would be an example using the fetch API:
+    /*
+    fetch("/your-api-endpoint", {
+      method: "POST",
+      body: formData,
+    })
+      .then(response => response.json())
+      .then(result => {
+        console.log("Success:", result);
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
+    */
+
+    // For now, just to check our FormData:
+    for (let pair of formData.entries()) {
+      console.log(pair[0] + ", " + pair[1]);
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <ImageField
+          file={file}
+          onFileChange={setFile}
+          previewImage={previewImage}
+          onPreviewImageChange={setPreviewImage}
+        />
+        <FormField
+          control={form.control}
+          name="location"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Location</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>Location of the event.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Cool event description."
+                  className="resize-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>
+                Tell us more about your awesome event.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Create Event</Button>
+      </form>
+    </Form>
+  );
+}
---
+++ frontend/src/components/ImageField.tsx
@@ -0,0 +1,72 @@
+"use client";
+import React from "react";
+import { Input } from "@/components/ui/input";
+
+interface ImageFieldProps {
+  file: File | undefined;
+  onFileChange: (file: File | undefined) => void;
+  previewImage: string | null;
+  onPreviewImageChange: (image: string | null) => void;
+  existingPreviewUrl?: string;
+}
+
+export function ImageField({
+  file,
+  onFileChange,
+  previewImage,
+  onPreviewImageChange,
+  existingPreviewUrl
+}: ImageFieldProps) {
+  
+  const FILE_SIZE_LIMIT = 2 * 1024 * 1024; // 2MB
+  const SUPPORTED_FILE_TYPES = ['image/jpeg', 'image/png'];
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0];
+
+    // File Size Validation
+    if (selectedFile && selectedFile.size > FILE_SIZE_LIMIT) {
+      alert("File size exceeds the 2MB limit.");
+      return;
+    }
+
+    // File Type Validation
+    if (selectedFile && !SUPPORTED_FILE_TYPES.includes(selectedFile.type)) {
+      alert("Unsupported file type. Please upload a JPEG or PNG image.");
+      return;
+    }
+
+    onFileChange(selectedFile);
+
+    if (selectedFile) {
+      const reader = new FileReader();
+
+      reader.onload = (event) => {
+        if (event.target && event.target.result) {
+          onPreviewImageChange(event.target.result as string);
+        }
+      };
+
+      reader.readAsDataURL(selectedFile);
+    } else {
+      onPreviewImageChange(null);
+    }
+  };
+
+  const displayedPreview = previewImage || existingPreviewUrl;
+
+  return (
+    <React.Fragment>
+      <Input type="file" name="file" onChange={handleImageChange} />
+      {displayedPreview && (
+        <div className="mt-4">
+          <img
+            src={displayedPreview}
+            alt="Profile Preview"
+            style={{ maxWidth: "200px" }}
+          />
+        </div>
+      )}
+    </React.Fragment>
+  );
+}
