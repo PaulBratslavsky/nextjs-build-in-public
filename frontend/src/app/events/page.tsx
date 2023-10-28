666c468a7b0c01d726abd25ec8c1637b14ad64b8
Anton ZTXONE <39146381+ztxone@users.noreply.github.com>
2023-10-28 03:44:41 +0300

    Added EventDetail layout and styling, updated css (#45)
    * Add Header component, add events page, add logo and naming
    
    * added Introduction section with stock images
    
    * update fetcher.ts
    
    * Update EventCard layout
    
    * updates of css, Added detail layout w/styling
    
    @ztxone you are awesome.

--- frontend/src/app/events/page.tsx
+++ frontend/src/app/events/page.tsx
@@ -1,7 +1,7 @@
 
 const EventsPage = () => {
   return (
-    <div className="container mx-auto px-8 flex items-center h-screen justify-center">EventsPage</div>
+    <div className="container mx-auto py-10 flex items-center h-screen justify-center">EventsPage</div>
   )
 }
 
--- frontend/src/components/ui/separator.tsx
+++ frontend/src/components/ui/separator.tsx
@@ -18,8 +18,8 @@ const Separator = React.forwardRef<
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        "shrink-0 bg-border",
-        orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
+        "shrink-0 bg-primary",
+        orientation === "horizontal" ? "h-[3px] w-[100px]" : "h-full w-[3px]",
         className
       )}
       {...props}
