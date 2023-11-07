d08cdcb5e19f8d6fb17b40d115ed05625ea66e57
Paul Bratslavsky <paul.bratslavsky@gmail.com>
2023-11-07 17:33:16 -0600

    fix app breaking when user is logout but the token is still present

--- frontend/src/context/AppContext.tsx
+++ frontend/src/context/AppContext.tsx
@@ -7,9 +7,9 @@ import React, {
   useEffect,
 } from "react";
 import meAction from "@/actions/me-action";
+import { useRouter } from "next/navigation";
 
 import type { StrapiAuthResponse } from "@/types/strapi-custom-types";
-
 
 interface MeActonResponse {
   data: StrapiAuthResponse;
@@ -19,25 +19,21 @@ interface MeActonResponse {
 
 interface AppContextType {
   user: StrapiAuthResponse | null;
-  setUser: React.Dispatch<React.SetStateAction<StrapiAuthResponse| null>>;
+  setUser: React.Dispatch<React.SetStateAction<StrapiAuthResponse | null>>;
 }
 
 const AppContext = createContext<AppContextType | undefined>(undefined);
 
-export const AppProvider: React.FC<{
-  children: React.ReactNode;
-  authUser: any;
-}> = ({ children }) => {
-  const [user, setUser] = useState<StrapiAuthResponse | null>( null);
-
+export const AppProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [user, setUser] = useState<StrapiAuthResponse | null>(null);
+  const router = useRouter();
   const fetchData = useCallback(async () => {
-    console.log("fetching data");
     try {
-      const response = await meAction() as MeActonResponse;
-      console.log("response", response);
-      if (response?.data) {
-        setUser(response.data);
-      }
+      const response = (await meAction()) as MeActonResponse;
+      if (response.data.error) return router.push("/signin");
+      if (response?.data.data) setUser(response.data);
     } catch (error) {
       console.error("Error fetching user data:", error);
     }
@@ -45,7 +41,7 @@ export const AppProvider: React.FC<{
 
   useEffect(() => {
     fetchData();
-  }, [ fetchData]);
+  }, [fetchData]);
 
   console.log("user", user);
   return (
