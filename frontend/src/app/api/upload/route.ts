c64b90b55452e1b1ee1d8781e4a0eb605daac279
Paul Bratslavsky <paul.bratslavsky@gmail.com>
2023-11-12 15:20:46 -0600

    update upload route

--- frontend/src/app/api/upload/route.ts
+++ frontend/src/app/api/upload/route.ts
@@ -1,27 +1,61 @@
 import { cookies } from 'next/headers'
 
-export async function POST(request: Request ) {
+export async function POST(request: Request): Promise<Response> {
   const url = `${process.env.STRAPI_URL}/api/upload`;
 
-  const cookieStore = cookies()
+  const cookieStore = cookies();
   const authToken = cookieStore.get('jwt')?.value;
 
-  if (!authToken) return { error: "No JWT", ok: false };
+  if (!authToken) {
+    return new Response(JSON.stringify({ error: "No JWT", ok: false }), { status: 401 });
+  }
 
-  const formData = await request.formData()
-  const file: File | null = formData.get('image') as unknown as File
+  const formData = await request.formData();
+  const file: File | null = formData.get('image') as unknown as File;
 
-  if (!file) return { error: "No file", ok: false };
+  if (!file) {
+    return new Response(JSON.stringify({ error: "No file", ok: false }), { status: 400 });
+  }
 
   const newFormData = new FormData();
   newFormData.append('files', file, file.name);
   
   const response = await fetch(url, {
     method: "POST",
-    headers: { Authorization: `Bearer ${authToken}`},
+    headers: { Authorization: `Bearer ${authToken}` },
     body: newFormData,
   });
 
   const data = await response.json();
-  return Response.json({ data: data[0], ok: true });
+  return new Response(JSON.stringify({ data: data[0], ok: true }), { status: response.status });
 }
+
+
+
+
+
+// export async function POST(request: Request ) {
+//   const url = `${process.env.STRAPI_URL}/api/upload`;
+
+//   const cookieStore = cookies()
+//   const authToken = cookieStore.get('jwt')?.value;
+
+//   if (!authToken) return { error: "No JWT", ok: false };
+
+//   const formData = await request.formData()
+//   const file: File | null = formData.get('image') as unknown as File
+
+//   if (!file) return { error: "No file", ok: false };
+
+//   const newFormData = new FormData();
+//   newFormData.append('files', file, file.name);
+  
+//   const response = await fetch(url, {
+//     method: "POST",
+//     headers: { Authorization: `Bearer ${authToken}`},
+//     body: newFormData,
+//   });
+
+//   const data = await response.json();
+//   return Response.json({ data: data[0], ok: true });
+// }
