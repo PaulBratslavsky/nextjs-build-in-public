126be8fcc7ba61b1eb052265370849593eef6d3d
Paul Bratslavsky <paul.bratslavsky@gmail.com>
2023-11-12 13:29:20 -0600

    updates

--- frontend/src/app/dashboard/profile/ProfileForm.tsx
+++ frontend/src/app/dashboard/profile/ProfileForm.tsx
@@ -1,121 +1,32 @@
 "use client";
 import { useState } from "react";
-import Link from "next/link";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useFieldArray, useForm } from "react-hook-form";
+import { useFormState } from "react-dom";
+import updateProfileAction from "@/actions/update-user-profile-action";
 import { ImageField } from "../../../components/ImageField";
-import * as z from "zod";
 
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import { Form} from "@/components/ui/form";
 import { FormInput } from "@/components/FormInput";
-
 import { Textarea } from "@/components/ui/textarea";
+import { getStrapiMedia } from "@/lib/api-helpers";
 
-const profileFormSchema = z.object({
-  username: z
-    .string()
-    .min(2, {
-      message: "Username must be at least 2 characters.",
-    })
-    .max(30, {
-      message: "Username must not be longer than 30 characters.",
-    }),
-  email: z
-    .string({
-      required_error: "Please select an email to display.",
-    })
-    .email(),
-  bio: z.string().max(160).min(4),
-  urls: z
-    .array(
-      z.object({
-        value: z.string().url({ message: "Please enter a valid URL." }),
-      })
-    )
-    .optional(),
-});
+export function ProfileForm({ userData }: { userData: any }) {
+  const initialState = null;
+  const { username, email, bio, urls } = userData.data;
+  const [state, dispatch] = useFormState(updateProfileAction, initialState);
 
-type ProfileFormValues = z.infer<typeof profileFormSchema>;
-
-// This can come from your database or API.
-const defaultValues: Partial<ProfileFormValues> = {
-  bio: "I own a computer.",
-  urls: [
-    { value: "https://shadcn.com" },
-    { value: "http://twitter.com/shadcn" },
-  ],
-};
-
-export function ProfileForm() {
-  const [file, setFile] = useState<File>();
-  const [previewImage, setPreviewImage] = useState<string | null>(null); // Step 1
-
-  const form = useForm<ProfileFormValues>({
-    resolver: zodResolver(profileFormSchema),
-    defaultValues,
-    mode: "onChange",
-  });
-
-
-  function onSubmit(data: ProfileFormValues) {
-    const formData = new FormData();
-  
-    // Append each form data
-    for (const [key, value] of Object.entries(data)) {
-      formData.append(key, value);
-    }
-  
-    // Append the file
-    if (file) {
-      formData.append('file', file); // 'file' here is the field name for the file, you can name it as per your backend's requirements
-    }
-  
-    // Assuming you would be making an HTTP request, this would be an example using the fetch API:
-    /*
-    fetch("/your-api-endpoint", {
-      method: "POST",
-      body: formData,
-    })
-      .then(response => response.json())
-      .then(result => {
-        console.log("Success:", result);
-      })
-      .catch(error => {
-        console.error("Error:", error);
-      });
-    */
-  
-    // For now, just to check our FormData:
-    for (let pair of formData.entries()) {
-      console.log(pair[0]+ ', ' + pair[1]);
-    }
-  }
-  
+  console.log(state), "from action";
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-2/3">
-        <ImageField
-          file={file}
-          onFileChange={setFile}
-          previewImage={previewImage}
-          onPreviewImageChange={setPreviewImage}
-        />
+    <form action={dispatch} className="space-y-8 w-2/3">
+      <FormInput
+        name="username"
+        label="Username"
+        placeholder="Username"
+        defaultValue={username}
+        data={state}
+      />
 
-        <FormInput 
-          name="username"
-          label="Username"
-          placeholder="Username"
-          data={form.formState}
-        />
-    
-     
- 
-
-        <Button type="submit">Update profile</Button>
-      </form>
-    </Form>
+      <Button type="submit">Update profile</Button>
+    </form>
   );
 }
--- frontend/src/components/ImageField.tsx
+++ frontend/src/components/ImageField.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Input } from "@/components/ui/input";
 
 interface ImageFieldProps {
+  name: string;
   file: File | undefined;
   onFileChange: (file: File | undefined) => void;
   previewImage: string | null;
@@ -12,6 +13,7 @@ interface ImageFieldProps {
 }
 
 export function ImageField({
+  name,
   file,
   onFileChange,
   previewImage,
@@ -25,6 +27,173 @@ export function ImageField({
 
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = e.target.files?.[0];
+
+    console.log(selectedFile, "qweweqrqwer");
+
+    // If no file is selected, set file to undefined
+    if (!selectedFile) {
+      onFileChange(undefined);
+      onPreviewImageChange(null);
+      return;
+    }
+
+    // File Size Validation
+    if (selectedFile.size > FILE_SIZE_LIMIT) {
+      alert("File size exceeds the 2MB limit.");
+      return;
+    }
+
+    // File Type Validation
+    if (!SUPPORTED_FILE_TYPES.includes(selectedFile.type)) {
+      alert("Unsupported file type. Please upload a JPEG or PNG image.");
+      return;
+    }
+
+    onFileChange(selectedFile);
+
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      if (event.target && event.target.result) {
+        onPreviewImageChange(event.target.result as string);
+      }
+    };
+    reader.readAsDataURL(selectedFile);
+  };
+
+  const displayedPreview = previewImage || existingPreviewUrl;
+
+  return (
+    <React.Fragment>
+      <Input name={name} type="file" onChange={handleImageChange} />
+      {displayedPreview && (
+        <div className="mt-4">
+          <img
+            src={displayedPreview}
+            alt="Profile Preview"
+            style={{ maxWidth: "200px" }}
+          />
+        </div>
+      )}
+    </React.Fragment>
+  );
+}
+
+
+// "use client";
+// import React from "react";
+// import { Input } from "@/components/ui/input";
+
+// interface ImageFieldProps {
+//   name: string;
+//   file: File | undefined;
+//   onFileChange: (file: File | undefined) => void;
+//   previewImage: string | null;
+//   onPreviewImageChange: (image: string | null) => void;
+//   existingPreviewUrl?: string;
+//   rest?: any;
+// }
+
+// export function ImageField({
+//   name,
+//   file,
+//   onFileChange,
+//   previewImage,
+//   onPreviewImageChange,
+//   existingPreviewUrl,
+//   ...rest
+// }: ImageFieldProps) {
+  
+//   const FILE_SIZE_LIMIT = 2 * 1024 * 1024; // 2MB
+//   const SUPPORTED_FILE_TYPES = ['image/jpeg', 'image/png'];
+
+//   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     const selectedFile = e.target.files?.[0];
+
+//     // File Size Validation
+//     if (selectedFile && selectedFile.size > FILE_SIZE_LIMIT) {
+//       alert("File size exceeds the 2MB limit.");
+//       return;
+//     }
+
+//     // File Type Validation
+//     if (selectedFile && !SUPPORTED_FILE_TYPES.includes(selectedFile.type)) {
+//       alert("Unsupported file type. Please upload a JPEG or PNG image.");
+//       return;
+//     }
+
+//     onFileChange(selectedFile);
+
+//     if (selectedFile) {
+//       const reader = new FileReader();
+
+//       reader.onload = (event) => {
+//         if (event.target && event.target.result) {
+//           onPreviewImageChange(event.target.result as string);
+//         }
+//       };
+
+//       reader.readAsDataURL(selectedFile);
+//     } else {
+//       onPreviewImageChange(null);
+//     }
+//   };
+
+//   const displayedPreview = previewImage || existingPreviewUrl;
+
+//   return (
+//     <React.Fragment>
+//       <Input name={name} type="file"  onChange={handleImageChange} { ...rest }/>
+//       {displayedPreview && (
+//         <div className="mt-4">
+//           <img
+//             src={displayedPreview}
+//             alt="Profile Preview"
+//             style={{ maxWidth: "200px" }}
+//           />
+//         </div>
+//       )}
+//     </React.Fragment>
+//   );
+// }
+
+
+/*
+
+"use client";
+import React, { useState } from "react";
+import { Input } from "@/components/ui/input";
+
+interface ImageFieldProps {
+  name: string;
+  file: File | undefined;
+  onFileChange: (file: File | undefined) => void;
+  previewImage: string | null;
+  onPreviewImageChange: (image: string | null) => void;
+  existingPreviewUrl?: string;
+  rest?: any;
+}
+
+export function ImageField({
+  name,
+  file,
+  onFileChange,
+  previewImage,
+  onPreviewImageChange,
+  existingPreviewUrl,
+  ...rest
+}: ImageFieldProps) {
+  
+  const FILE_SIZE_LIMIT = 2 * 1024 * 1024; // 2MB
+  const SUPPORTED_FILE_TYPES = ['image/jpeg', 'image/png'];
+
+  // State to track if the image has been modified
+  const [imageModified, setImageModified] = useState(false);
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0];
+
+    // Set imageModified to true if a new file is selected
+    setImageModified(!!selectedFile);
 
     // File Size Validation
     if (selectedFile && selectedFile.size > FILE_SIZE_LIMIT) {
@@ -57,9 +226,24 @@ export function ImageField({
 
   const displayedPreview = previewImage || existingPreviewUrl;
 
+  // Function to prepare form data for submission
+  const prepareFormData = () => {
+    const formData = new FormData();
+
+    // Add image to form data only if it has been modified
+    if (imageModified) {
+      formData.append(name, file);
+    }
+
+    // Add other form data as needed
+    // ...
+
+    return formData;
+  };
+
   return (
     <React.Fragment>
-      <Input type="file"  onChange={handleImageChange} { ...rest }/>
+      <Input name={name} type="file" onChange={handleImageChange} {...rest}/>
       {displayedPreview && (
         <div className="mt-4">
           <img
@@ -72,3 +256,4 @@ export function ImageField({
     </React.Fragment>
   );
 }
+*/
