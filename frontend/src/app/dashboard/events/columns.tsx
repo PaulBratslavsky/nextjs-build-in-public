073115eca86acf937a77af9e107d24d8491da8d3
Paul Bratslavsky <paul.bratslavsky@gmail.com>
2023-11-17 02:18:08 -0600

    added soft delete and restore

--- frontend/src/app/dashboard/events/columns.tsx
+++ frontend/src/app/dashboard/events/columns.tsx
@@ -1,11 +1,12 @@
-"use client"
+"use client";
 
-import { ColumnDef } from "@tanstack/react-table"
-import { ArchiveRestore, ArchiveX, Pencil } from "lucide-react"
-import { formatTime } from "@/lib/api-helpers"
-import { StrapiEventData } from "@/types/strapi-custom-types"
+import { ColumnDef } from "@tanstack/react-table";
+import { ArchiveRestore, ArchiveX, Pencil } from "lucide-react";
+import { formatTime } from "@/lib/api-helpers";
+import { StrapiEventData } from "@/types/strapi-custom-types";
 import Link from "next/link";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { updateEventOnServer } from "@/lib/utils";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -18,12 +19,54 @@ import { Button } from "@/components/ui/button"
 //   time: string
 // }
 
+function ArchiveButton({ eventId }: { eventId: string }) {
+  async function handleClick() {
+    const eventFormData = {
+      data: { status: "ARCHIVED" },
+    };
+    await updateEventOnServer(eventFormData, eventId);
+  }
+  return (
+    <Button
+      onClick={handleClick}
+      size="icon"
+      variant="ghost"
+      className="hover:text-white"
+    >
+      <ArchiveX className="h-5 w-5" />
+    </Button>
+  );
+}
+
+function RestoreButton({ eventId }: { eventId: string }) {
+  async function handleClick() {
+    const eventFormData = {
+      data: { status: "DRAFT" },
+    };
+    await updateEventOnServer(eventFormData, eventId);
+  }
+  return (
+    <Button
+      onClick={handleClick}
+      size="icon"
+      variant="ghost"
+      className="hover:text-white"
+    >
+      <ArchiveRestore className="h-5 w-5" />
+    </Button>
+  );
+}
 
 export const columns: ColumnDef<StrapiEventData>[] = [
   {
     id: "title",
     accessorKey: "attributes.title",
     header: "Title",
+  },
+  {
+    id: "status",
+    accessorKey: "attributes.status",
+    header: "Status",
   },
   {
     id: "location",
@@ -40,22 +83,25 @@ export const columns: ColumnDef<StrapiEventData>[] = [
     accessorKey: "attributes.time",
     header: "Time",
     cell: ({ row }) => {
-      const time = row.original.attributes.time        
-      return <div>{formatTime(time)}</div>
+      const time = row.original.attributes.time;
+      return <div>{formatTime(time)}</div>;
     },
   },
   {
     id: "actions",
     header: "Actions",
     cell: ({ row }) => {
-      console.log(row.original.id)
       return (
         <div className="flex gap-1">
-        <Link href={`events/${row.original.id}`}><Button size="icon" variant="ghost" className="hover:text-white"><Pencil className="h-4 w-4" /></Button></Link>
-        <Button size="icon" variant="ghost" className="hover:text-white"><ArchiveX className="h-5 w-5" /></Button>
-        <Button size="icon" variant="ghost" className="hover:text-white"><ArchiveRestore className="h-5 w-5" /></Button>
+          <Link href={`events/${row.original.id}`}>
+            <Button size="icon" variant="ghost" className="hover:text-white">
+              <Pencil className="h-4 w-4" />
+            </Button>
+          </Link>
+          <ArchiveButton eventId={String(row.original.id)} />
+          <RestoreButton eventId={String(row.original.id)} />
         </div>
-      )
+      );
     },
   },
-]
+];
--- frontend/src/app/dashboard/events/page.tsx
+++ frontend/src/app/dashboard/events/page.tsx
@@ -1,47 +1,55 @@
-
 import qs from "qs";
 import { Suspense } from "react";
 import type { StrapiEventData } from "@/types/strapi-custom-types";
 import getEventsAuthLoader from "@/loaders/get-events-auth-loader";
-import { Archive } from "lucide-react"
+import { Archive } from "lucide-react";
 
-import {  columns } from "./columns";
+import { columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
 import PageHeading from "@/components/PageHeading";
-import { Separator } from "@/components/ui/separator"
+import { Separator } from "@/components/ui/separator";
 
 const eventsQuery = qs.stringify({
-  populate: {
-    image: {
-      fields: ["url", "alternativeText"],
+  filters: {
+    status: {
+      $ne: "ARCHIVED",
     },
   },
   sort: ["date:desc"],
 });
 
+const eventsArchivedQuery = qs.stringify({
+  filters: {
+    status: {
+      $eq: "ARCHIVED",
+    },
+  },
+  sort: ["date:desc"],
+});
 
 export default async function MyEventsRoute() {
-
   const resEvents = await getEventsAuthLoader(eventsQuery);
   const events = resEvents?.data.data as StrapiEventData[];
-  const empty = [] as StrapiEventData[]
+
+  const resEventsArchived = await getEventsAuthLoader(eventsArchivedQuery);
+  const eventsArchived = resEventsArchived?.data.data as StrapiEventData[];
+
   if (!events) return null;
-  
+
   return (
     <Suspense fallback={<div>Loading...</div>}>
-    <div className="space-y-6">
-      <PageHeading heading="My Events" subheading="Manage your events." />
-      <DataTable columns={columns} data={events} />
-    </div>
-    <Separator className="my-10" />
-    <div>
-      <h3 className="text-sm flex gap-2">
-        <Archive className="h-5 w-5"/>Your passed and archived events</h3>
-      <DataTable columns={columns} data={empty} />
-    </div>
+      <div className="space-y-6">
+        <PageHeading heading="My Events" subheading="Manage your events." />
+        <DataTable columns={columns} data={events} />
+      </div>
+      <Separator className="my-10" />
+      <div>
+        <h3 className="text-sm flex gap-2">
+          <Archive className="h-5 w-5" />
+          Your passed and archived events
+        </h3>
+        <DataTable columns={columns} data={eventsArchived} />
+      </div>
     </Suspense>
   );
 }
-
-
-
