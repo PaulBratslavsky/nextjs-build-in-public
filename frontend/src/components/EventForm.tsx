e283d4614448fe4f1e8dbeed9808e6b5688b18a7
Paul Bratslavsky <paulbratslavsky@Pauls-MacBook-Pro.local>
2023-11-20 14:27:57 -0600

    Fixed callander to pick only future dates for adding events

--- frontend/src/components/EventForm.tsx
+++ frontend/src/components/EventForm.tsx
@@ -111,9 +111,7 @@ export default function EventForm({
                     mode="single"
                     selected={field.value}
                     onSelect={field.onChange}
-                    disabled={(date) =>
-                      date > new Date() || date < new Date("1900-01-01")
-                    }
+                    disabled={(date) => date < new Date()}
                     initialFocus
                   />
                 </PopoverContent>
